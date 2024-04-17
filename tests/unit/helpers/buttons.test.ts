c257cea6849265d163e425761d4baec3686eb93c
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-17 17:31:11 +0200

    Added test to assert correct pair buttons configuration with same pair id is generated

--- tests/unit/helpers/buttons.test.ts
+++ tests/unit/helpers/buttons.test.ts
@@ -7,5 +7,24 @@ describe("Buttons", () => {
         createButtonsPairData(["Denmark", "Copenhagen", "Norway", "Oslo"]);
       }).toThrow(new Error("Labels need to be a pair"));
     });
+
+    it("should return buttons pair with correct configuration and same pair id", () => {
+      const buttonsPair = createButtonsPairData(["Denmark", "Copenhagen"]);
+      const [countryButtonData, capitalButtonData] = buttonsPair;
+
+      expect(countryButtonData.pairId).toBe(capitalButtonData.pairId);
+
+      expect(countryButtonData).toEqual({
+        id: expect.any(String),
+        pairId: expect.any(String),
+        label: "Denmark",
+      });
+
+      expect(capitalButtonData).toEqual({
+        id: expect.any(String),
+        pairId: expect.any(String),
+        label: "Copenhagen",
+      });
+    });
   });
 });
