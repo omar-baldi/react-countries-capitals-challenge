7ccf7a28b4006b84c6f41f08ea8877aceec922c2
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-18 09:01:43 +0200

    Added test to assert selected buttons pair background color property is set to red if invalid

--- tests/unit/components/CountryCapitalGame.test.tsx
+++ tests/unit/components/CountryCapitalGame.test.tsx
@@ -34,4 +34,24 @@ describe("CountryCapitalGame", () => {
     fireEvent.click(buttonDenmark);
     expect(buttonDenmark).toHaveStyle({ backgroundColor: "#4009FF" });
   });
+
+  it("should update background color to 'red' for selected buttons when pair is invalid", () => {
+    const wrapper = render(
+      <CountryCapitalGame
+        data={{
+          Denmark: "Copenhagen",
+          Italy: "Rome",
+        }}
+      />
+    );
+
+    const buttonDenmark = wrapper.getByTestId("btn-denmark", { exact: false });
+    const buttonRome = wrapper.getByTestId("btn-rome", { exact: false });
+
+    fireEvent.click(buttonDenmark);
+    fireEvent.click(buttonRome);
+
+    expect(buttonDenmark).toHaveStyle({ backgroundColor: "red" });
+    expect(buttonRome).toHaveStyle({ backgroundColor: "red" });
+  });
 });
