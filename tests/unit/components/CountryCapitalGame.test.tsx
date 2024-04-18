7e2eeb900fed84b1b9fbac5f0548d705616bf864
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-18 09:00:31 +0200

    Added test to assert correct background color property update for selected button

--- tests/unit/components/CountryCapitalGame.test.tsx
+++ tests/unit/components/CountryCapitalGame.test.tsx
@@ -1,6 +1,6 @@
 import CountryCapitalGame from "@/components/CountryCapitalGame";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 describe("CountryCapitalGame", () => {
   it("should all buttons have correct background color property on component mount", () => {
@@ -18,5 +18,20 @@ describe("CountryCapitalGame", () => {
     for (const buttonElement of buttonsElements) {
       expect(buttonElement).toHaveStyle({ backgroundColor: "initial" });
     }
+  });
+
+  it("should update button background color when triggering a click", () => {
+    const wrapper = render(
+      <CountryCapitalGame
+        data={{
+          Denmark: "Copenhagen",
+          Italy: "Rome",
+        }}
+      />
+    );
+
+    const buttonDenmark = wrapper.getByTestId("btn-denmark", { exact: false });
+    fireEvent.click(buttonDenmark);
+    expect(buttonDenmark).toHaveStyle({ backgroundColor: "#4009FF" });
   });
 });
