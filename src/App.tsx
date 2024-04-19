51cd21f675c6df93cbcce0056b4888604e4853ae
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-19 12:42:02 +0200

    Rendered component in entrypoint file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import CountryCapitalGame from "./components/CountryCapitalGame";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <CountryCapitalGame
+        data={{
+          Denmark: "Copenhagen",
+          Italy: "Rome",
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
