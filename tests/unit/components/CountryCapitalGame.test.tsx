e1cd1a7ec6c58bceb9e86b45d0b4b42207aafb71
Omar Baldi <omar.baldi.business@gmail.com>
2024-04-19 12:39:30 +0200

    Added tests to assert correct background color change behaviour on single and pair buttons click

--- tests/unit/components/CountryCapitalGame.test.tsx
+++ tests/unit/components/CountryCapitalGame.test.tsx
@@ -1,6 +1,6 @@
 import CountryCapitalGame from "@/components/CountryCapitalGame";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 describe("CountryCapitalGame", () => {
   it("should all buttons have correct background color property on component mount", () => {
@@ -18,5 +18,40 @@ describe("CountryCapitalGame", () => {
     for (const buttonElement of buttonsElements) {
       expect(buttonElement).toHaveStyle({ backgroundColor: "initial" });
     }
+  });
+
+  it("should update button background color when triggering a click", () => {
+    const wrapper = render(
+      <CountryCapitalGame
+        data={{
+          Denmark: "Copenhagen",
+          Italy: "Rome",
+        }}
+      />
+    );
+
+    const buttonDenmark = wrapper.getByTestId("btn-denmark", { exact: false });
+    fireEvent.click(buttonDenmark);
+    expect(buttonDenmark).toHaveStyle({ backgroundColor: "#4009FF" });
+  });
+
+  it("should update background color to 'red' for selected buttons when pair is invalid", () => {
+    const wrapper = render(
+      <CountryCapitalGame
+        data={{
+          Denmark: "Copenhagen",
+          Italy: "Rome",
+        }}
+      />
+    );
+
+    const buttonDenmark = wrapper.getByTestId("btn-denmark", { exact: false });
+    const buttonRome = wrapper.getByTestId("btn-rome", { exact: false });
+
+    fireEvent.click(buttonDenmark);
+    fireEvent.click(buttonRome);
+
+    expect(buttonDenmark).toHaveStyle({ backgroundColor: "red" });
+    expect(buttonRome).toHaveStyle({ backgroundColor: "red" });
   });
 });
